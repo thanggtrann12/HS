d46d0ade980dd396d2f2134361787a8edf100ec5
rhn9hc <thang.trantrung@vn.bosch.com>
2023-11-14 13:55:24 +0700

    add lib/gtest

---
+++ lib/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.obj"
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.obj.d"
+  "bin/gtestpdb_debug_postfix-NOTFOUND.pdb"
+  "lib/gtest.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
